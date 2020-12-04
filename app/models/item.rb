0fd9e357f19f0bdf99b293ba50f85a961e1d0208
airi32490 <milcrea313@gmail.com>
2020-12-04 14:51:35 +0900

    商品情報の保存

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,23 +1,29 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :status, :area, :day, :fee
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :area
+  belongs_to_active_hash :day
+  belongs_to_active_hash :fee
 
   belongs_to :user
   
   has_one_attached :image
 
-  with_options numericality: {other_than: 1} do
-    validates :category
-    validates :status
-    validates :area
-    validates :day
-    validates :fee
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :describe
+    validates :price, numericality: { only_integer: true, message: "is invalid. Input half-width characters." }
   end
 
-  with_options precense: true do
-    validates :name
-    validates :price, numericality: { only_integer: true, message: "is invalid. Input half-width characters." }
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
-    validates :describe
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+
+  with_options numericality: {other_than: 1, message: "Select"} do
+    validates :category_id
+    validates :status_id
+    validates :fee_id
+    validates :area_id
+    validates :day_id
   end
 end
