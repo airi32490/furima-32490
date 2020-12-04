3ea9c6d11954c51c97833e34177b670e1557e0fe
airi32490 <milcrea313@gmail.com>
2020-12-04 12:10:08 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,19 @@ class Item < ApplicationRecord
   belongs_to :user
   
   has_one_attached :image
+
+  with_options numericality: {other_than: 1} do
+    validates :category
+    validates :status
+    validates :area
+    validates :day
+    validates :fee
+  end
+
+  with_options precense: true do
+    validates :name
+    validates :price, numericality: { only_integer: true, message: "is invalid. Input half-width characters." }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+    validates :describe
+  end
 end
