4c28e3e26bbb9a34330034f2dce26b3bfe83ca0d
airi32490 <milcrea313@gmail.com>
2020-12-04 18:01:25 +0900

    itemモデルの単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name        { 'テスト' }
+    price       { 5000 }
+    describe    { 'これはテストです'}
+    category_id { 5 }
+    status_id   { 2 }
+    area_id     { 10 }
+    day_id      { 2 }
+    fee_id      { 3 }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test.jpg'), filename: 'test.jpg')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,85 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品の出品' do
+    context '商品の出品が成功する時' do
+      it 'name, price, describe, image, category_id, status_id,
+          area_id, day_id, fee_id, user_idが全て存在すれば出品できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品の出品が失敗する時' do
+      it 'imageがないと出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'nameが入力されていないと出品できない' do
+        @item.name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'describeが入力されていないと出品できない' do
+        @item.describe = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Describe can't be blank")
+      end
+      it 'category_idが選択されていないと出品できない' do
+        @item.category_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category Select")
+      end
+      it 'status_idが選択されていないと出品できない' do
+        @item.status_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status Select")
+      end
+      it 'fee_idが選択されていないと出品できない' do
+        @item.fee_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Fee Select")
+      end
+      it 'area_idが選択されていないと出品できない' do
+        @item.area = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area Select")
+      end
+      it 'day_idが選択されていないと出品できない' do
+        @item.day_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Day Select")
+      end
+      it 'priceが入力されていないと出品できない' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceが¥300円未満だと出品できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is out of setting range")
+      end
+      it 'priceが¥10,000,000以上だと出品できない' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is out of setting range")
+      end
+      it 'priceが全角数字だと出品できない' do
+        @item.price = '１００００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is invalid. Input half-width characters.",
+                                                      "Price is out of setting range")
+      end
+      it 'ログインしていないユーザーは出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
