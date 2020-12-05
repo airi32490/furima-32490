26689115e51b81a0c9cc9e18a36e3f96736367fa
airi32490 <milcrea313@gmail.com>
2020-12-05 21:10:49 +0900

    @item.valid?を削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,7 +11,6 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.valid?
     if @item.save
       redirect_to root_path
     else
@@ -29,7 +28,6 @@ class ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
-    @item.valid?
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
