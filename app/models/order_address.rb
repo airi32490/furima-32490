d4ae9bd35477a9e4796b6eee881080d7438b3bd8
airi32490 <milcrea313@gmail.com>
2020-12-07 14:25:25 +0900

    データを保存するメソッドを定義

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -9,4 +9,9 @@ class OrderAddress
   validates :house_number
   validates :tel_number, format: { with: /\A\d{11}\z/, message: "Input only number" }
   end
+
+  def save
+    order = Order.create(user_id: user.id, item_id: item.id)
+    Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, order_id: order.id)
+  end
 end
