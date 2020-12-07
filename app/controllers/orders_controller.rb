d53a284cee92b4d1e85e19601bf3e0f5074e0063
airi32490 <milcrea313@gmail.com>
2020-12-07 21:52:28 +0900

    購入情報保存の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,31 @@
 class OrdersController < ApplicationController
-
+  before_action :authenticate_user!, only: [:index]
+  before_action :move_to_index, only: [:index]
   def index
+    @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
   end
-  
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :tel_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def move_to_index
+    item = Item.find(params[:item_id])
+    if user_signed_in? && current_user.id == item.user_id
+      redirect_to root_path
+    end
+  end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,17 +1,18 @@
 class OrderAddress
-  include ActiveModel::ActiveModel
-  attr_accessor :postal_cade, :prefecture_id, :city, :house_number, :building_name, :tel_number
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :tel_number, :user_id, :item_id
 
-  with_options precense :true do
-  validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "Input correctly"}
-  validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
-  validates :city
-  validates :house_number
-  validates :tel_number, format: { with: /\A\d{11}\z/, message: "Input only number" }
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'Input correctly' }
+    validates :city
+    validates :house_number
+    validates :tel_number, format: { with: /\A\d{11}\z/, message: 'Input only number' }
   end
 
+  validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
+
   def save
-    order = Order.create(user_id: user.id, item_id: item.id)
-    Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, order_id: order.id)
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, tel_number: tel_number, order_id: order.id)
   end
 end
