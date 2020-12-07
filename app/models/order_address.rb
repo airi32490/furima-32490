d44b9edec5c647ad28126a0767f5edffe412945c
airi32490 <milcrea313@gmail.com>
2020-12-07 14:04:10 +0900

    バリデーションの設定

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,4 +1,12 @@
 class OrderAddress
   include ActiveModel::ActiveModel
   attr_accessor :postal_cade, :prefecture_id, :city, :house_number, :building_name, :tel_number
+
+  with_options precense :true do
+  validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "Input correctly"}
+  validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
+  validates :city
+  validates :house_number
+  validates :tel_number, format: { with: /\A\d{11}\z/, message: "Input only number" }
+  end
 end
